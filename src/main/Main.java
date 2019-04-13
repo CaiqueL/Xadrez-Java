53dd6ed91b287ebb9387f5802c326e4c600cc252
Caique Lopes <cacalps@gmail.com>
2019-04-13 16:23:45 -0300

    implementação da UI/ChessMatch

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import Chess.ChessMatch;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 
 	}
 
